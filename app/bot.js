b45128e86cde8440f700924f31b8eaca9e098dcb
Andrés <andresbonojimenez@gmail.com>
2016-07-30 18:38:00 +0200

    [FEATURE] Internationalize messages to different languages

--- app/bot.js
+++ app/bot.js
@@ -2,6 +2,22 @@ var Twit = require('twit'); // Twitter API Client See: https://www.npmjs.com/pac
 
 const TWEETLEN = 140;
 
+const NEW_USERNAME = '';
+
+// Internationalization
+const DEFAULT_LANG = 'en'; // The default language must exist in textReply and textDM objects.
+
+const textReply = {
+  en: `follow @${NEW_USERNAME}, please.`,
+  es: `sigue a @${NEW_USERNAME}, por favor.`
+};
+
+const textDM = {
+  en: 'Thanks for following me.',
+  es: 'Gracias por seguirme.'
+};
+
+
 // import secret.json file
 var secret = require("./secret");
 /* that should look like this:
@@ -29,11 +45,11 @@ T.get('account/verify_credentials', null, function(err, data, response) {
     // User Stream
     var stream = T.stream('user');
 
-    // stream.on('message', function (msg) { console.log("[INFO] Msg received\n", msg) });
+    stream.on('message', function (msg) { console.log("[INFO] Msg received\n", msg) });
 
     // Emitted when the response is received from Twitter.
     stream.on('connected', function () {
-      console.log('[INFO] Stream connected')
+      console.log('[INFO] Stream connected');
     })
 
     // follow event handler
@@ -42,10 +58,14 @@ T.get('account/verify_credentials', null, function(err, data, response) {
         var newfollower = eventMsg.source.screen_name;
         console.log("[STREAM] New follower: @"+newfollower);
 
-        // Send a Direct Message. See: https://dev.twitter.com/rest/reference/post/direct_messages/new
-        var textDM = "Thanks for following me."
+        // Get source language and select text
+        var lang = eventMsg.source.lang;
+        if (! textDM.hasOwnProperty(lang)){ // If property doesn't exist
+          lang = DEFAULT_LANG;
+        }
 
-        T.post('direct_messages/new', { screen_name: newfollower, text: textDM }, function(err, data, response) {
+        // Send a Direct Message. See: https://dev.twitter.com/rest/reference/post/direct_messages/new
+        T.post('direct_messages/new', { screen_name: newfollower, text: textDM[lang] }, function(err, data, response) {
           if(!err) {
             console.log("[STREAM] DM sent to @"+newfollower);
           } else {
@@ -64,9 +84,16 @@ T.get('account/verify_credentials', null, function(err, data, response) {
       if (mentionsMe(SCREEN_NAME, tweet.entities.user_mentions) && ! (sender === SCREEN_NAME) ) {
         console.log("[STREAM] New mention from: @"+sender);
 
+        // Get detected language or user language
+        var lang = DEFAULT_LANG;
+        if (textDM.hasOwnProperty(tweet.lang)){ // If detected language exists
+          lang = tweet.lang;
+        } else if (textDM.hasOwnProperty(tweet.user.lang)) {  // If user language exists
+          lang = tweet.user.lang;
+        }
+
         // Send a reply. See: in_reply_to_status_id https://dev.twitter.com/rest/reference/post/statuses/update
-        var textReply = "please follow other";
-        var reply = makeReply(sender, tweet.entities.user_mentions, textReply, SCREEN_NAME);
+        var reply = makeReply(sender, tweet.entities.user_mentions, textReply[lang], SCREEN_NAME);
 
         // NOTE: "when working with JavaScript in particular, please make sure
         // you use the stringified IDs id_str instead of id to avoid any integer
@@ -97,7 +124,7 @@ T.get('account/verify_credentials', null, function(err, data, response) {
 
     // Emitted when a reconnection attempt to Twitter is scheduled. If Twitter is having problems or we get rate limited, we schedule a reconnect according to Twitter's reconnection guidelines. The last http request and response objects are emitted, along with the time (in milliseconds) left before the reconnect occurs.
     stream.on('reconnect', function (req, res, connectInterval) {
-      console.log('[WARN] Got disconnected. Scheduling reconnect')
+      console.log('[WARN] Got disconnected. Scheduling reconnect');
     });
 
   } else {
