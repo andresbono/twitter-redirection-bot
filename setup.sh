72291dd1923ca3c6eb2132a434c03084fa4ee64b
Andrés <andresbonojimenez@gmail.com>
2016-08-02 20:51:33 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# TODO Check if nodejs is installed
+
+# Install Twitter API Client for node https://www.npmjs.com/package/twit
+echo "Adding dependencies..."
+npm install twit
