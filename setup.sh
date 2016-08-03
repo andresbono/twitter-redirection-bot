e29af757fd9d853ee37992474204b43e98f8c2ab
Andrés <andresbonojimenez@gmail.com>
2016-08-03 17:48:07 +0200

    Add package.json file (very incomplete status)
    Also: 'npm install' command reads this file now

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@
 # TODO Check if nodejs is installed
 
 # Install Twitter API Client for node https://www.npmjs.com/package/twit
+# Dependencies are listed in the package.json file
 echo "Adding dependencies..."
-npm install twit
+npm install
